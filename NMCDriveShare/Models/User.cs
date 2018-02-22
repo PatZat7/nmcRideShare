6ba2e5b455e78e5ef467e9d830839dfe7f2d0ec7
g1hanna <g1hanna@icloud.com>
2018-02-22 12:50:18 -0500

    Rebuilt ADO objects and models
    * Reimported database schema using Entity model Diagram in Visual Studio

--- NMCDriveShare/Models/User.cs
+++ NMCDriveShare/Models/User.cs
@@ -14,20 +14,32 @@ namespace NMCDriveShare.Models
     
     public partial class User
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public User()
+        {
+            this.Comments = new HashSet<Comment>();
+            this.RideBoards = new HashSet<RideBoard>();
+            this.RideBoardUsers = new HashSet<RideBoardUser>();
+        }
+    
         public int userID { get; set; }
         public string lastName { get; set; }
         public string firstName { get; set; }
         public string username { get; set; }
         public string password { get; set; }
-        public string streetName { get; set; }
-        public string city { get; set; }
-        public string state { get; set; }
-        public string zip { get; set; }
         public string phoneNumber { get; set; }
-        public Nullable<bool> isRider { get; set; }
         public Nullable<bool> isDriver { get; set; }
-        public Nullable<double> rating { get; set; }
         public string gender { get; set; }
         public string nmcEmail { get; set; }
+        public Nullable<int> locationId { get; set; }
+        public bool isActive { get; set; }
+    
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Comment> Comments { get; set; }
+        public virtual Geolocation Geolocation { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<RideBoard> RideBoards { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<RideBoardUser> RideBoardUsers { get; set; }
     }
 }
