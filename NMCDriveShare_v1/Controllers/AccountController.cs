755bfa867f568f223902c94d95461bb43579e926
g1hanna <g1hanna@icloud.com>
2018-03-08 12:02:01 -0500

    Rebuild code and remove weird changes

--- NMCDriveShare_v1/Controllers/AccountController.cs
+++ NMCDriveShare_v1/Controllers/AccountController.cs
@@ -161,29 +161,7 @@ namespace NMCDriveShare_v1.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-
-                if (result.Succeeded)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-
-                    //TEMPORARY: Global Var to track login status of user
-                    /////////
-                    this.Session["loggedIn"] = true;
-                    ////////
-
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Home");
-                }
-                AddErrors(result);
-=======
-				try
+                try
 				{
 					// create new AspNetUser
 					var aspNetUser = new ApplicationUser { UserName = model.Email, Email = model.Email };
