5a2080fdad18549e6952df035b0f9a9b6726f94a
g1hanna <g1hanna@icloud.com>
2018-03-20 16:17:31 -0400

    Restored changes for Google Maps
    + Re-added Google Maps
    + Verified sign-in and map functionality

--- NMCDriveShare_v1/Controllers/PortalController.cs
+++ NMCDriveShare_v1/Controllers/PortalController.cs
@@ -1,17 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace NMCDriveShare_v1.Controllers
+namespace NMCDriveShare.Controllers
 {
     public class PortalController : Controller
     {
-        // GET: Portal
-        public ActionResult Index()
-        {
-            return View();
-        }
+		// Restored changes from a previous commit
+		// Users display portal
+		[Authorize]
+		public ActionResult Index()
+		{
+			// Connects to database through SqlClient
+			// TODO: Replace SqlClient code with Entity Frameword code
+
+			// store markers in a JSON array
+			string markers = "[\n";
+			string connectionString = ConfigurationManager.ConnectionStrings["DriveShareEntities3"].ConnectionString;
+			SqlCommand selector = new SqlCommand("SELECT * FROM vw_UserLocations");
+
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				// bind the statement to the connection
+				selector.Connection = connection;
+
+				// open the connection
+				connection.Open();
+
+				using (SqlDataReader dataReader = selector.ExecuteReader())
+				{
+					// read the view one item at a time
+					while (dataReader.Read())
+					{
+						// get fields ready for a JSON object
+						string name = dataReader["firstName"] as string + dataReader["lastName"] as string;
+						bool isDriver = Convert.ToBoolean(dataReader["isDriver"]);
+						string description = "";
+
+						if (isDriver)
+							description = "Driver";
+						else
+							description = "Rider";
+
+						// generate a JSON object for each item in the DB View
+						string marker = "{\n";
+
+						marker += $"'title': '{name}'";
+						marker += $"'lat': '{dataReader["latitude"]}'";
+						marker += $"'lng': '{dataReader["longitude"]}'";
+						marker += $"'description': '{description}'";
+
+						marker += "},\n";
+
+						// add the marker to the marker array
+						markers += marker;
+					}
+				}
+
+				// close the connection
+				connection.Close();
+			}
+
+			markers += "]";
+
+			// send JSON off to the View
+			ViewBag.markers = markers;
+
+			return View();
+		}
     }
 }
